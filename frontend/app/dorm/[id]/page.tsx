351e4028284e6ebad54272ce77bf29a7e6c150ad
BantitaC <bantita.chk@gmail.com>
2025-09-16 23:06:36 +0700

    frontend update userdorm

--- frontend/app/dorm/[id]/page.tsx
+++ frontend/app/dorm/[id]/page.tsx
@@ -1,8 +1,13 @@
+"use client"
 import Navbar from "@/app/components/Navbar";
+import { useState } from "react";
 import Link from "next/link"
 import Image from "next/image";
+import Reported from "@/app/components/dorm/Reported";
+import Completed from "@/app/components/dorm/Completed";
+export default function Dorm() {
+    const [page, setPage] = useState(0);
 
-export default function UserMain() {
 
     return (
         <div>
@@ -12,12 +17,14 @@ export default function UserMain() {
                 <div className="col-span-1"></div>
 
                 <div className="col-span-10">
+
                     <div className="flex items-center justify-between">
                         <h1 className="text-[#3674B5] font-bold text-5xl">Dormitory A</h1>
                         <Link href="/report-form" className="flex items-center space-x-2">
                             <button
                                 type="button"
                                 className="px-4 py-2 bg-[#3674B5] text-white rounded-xl hover:bg-[#E61D1D] transition cursor-pointer"
+
                             >
                                 Report New Issue
                             </button>
@@ -28,7 +35,9 @@ export default function UserMain() {
                         <div className="col-span-5">
                             <button
                                 type="button"
-                                className="w-full h-full px-4 py-2 bg-[#3674B5] text-white text-xl font-bold rounded-xl hover:bg-sky-600 transition cursor-pointer"
+                                className={`w-full h-full px-4 py-2  text-xl font-bold rounded-xl transition cursor-pointer
+    ${page === 1 ? "bg-[#EFEFEF] text-[#9F9F9F]" : "bg-[#3674B5] text-white hover:bg-sky-600"}`}
+                                onClick={() => setPage(0)}
                             >
                                 Reported
                             </button>
@@ -36,7 +45,10 @@ export default function UserMain() {
                         <div className="col-span-5">
                             <button
                                 type="button"
-                                className="w-full h-full px-4 py-2 bg-[#EFEFEF] text-[#9F9F9F] text-xl font-bold rounded-xl hover:bg-gray-300 transition cursor-pointer"
+                                className={`w-full h-full px-4 py-2  text-xl font-bold rounded-xl transition cursor-pointer
+    ${page === 0 ? "bg-[#EFEFEF] text-[#9F9F9F]" : "bg-[#3674B5] text-white hover:bg-sky-600"}`}
+                                
+                                onClick={() => setPage(1)}
                             >
                                 Completed
                             </button>
@@ -57,6 +69,13 @@ export default function UserMain() {
                         </div>
 
                     </div>
+                    {page == 0 ?
+                        <Reported />
+                        :
+                        <Completed />
+                    }
+
+
                 </div>
 
 
